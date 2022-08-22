bfe58a86c4ac4a84043d1bc3343809e52ba56631
m_mitin <maximus405@gmail.com>
2022-08-22 20:03:39 +0300

    create new func to test selected category

--- chuck_random.py
+++ chuck_random.py
@@ -29,5 +29,32 @@ class Test_new_joke():
         else:
             print('ERROR!!!!!!')
 
+    def create_new_joke_category(self, category):
+        """Create joke for category ["animal","career","celebrity","dev",
+        "explicit","fashion","food","history","money","movie","music","political",
+        "religion","science","sport","travel"] """
+        
+        url = f'https://api.chucknorris.io/jokes/random?category={category}'
+        result = requests.request('get', url)
+        assert 200 == result.status_code
+        if result.status_code == 200:
+            print('Success')
+        else:
+            print('Failed')
+        result.encoding = 'utf-8'
+        print(result.text)
+        check = result.json()
+        check_cat = check.get("categories")
+        if check_cat == [f"{category}"]:
+            print('Category sport')
+        else:
+            print('Error')
+
+
+
+
 random_joke = Test_new_joke()
 random_joke.create_new_joke()
+
+sport_joke = Test_new_joke()
+sport_joke.create_new_joke_category('sport')
